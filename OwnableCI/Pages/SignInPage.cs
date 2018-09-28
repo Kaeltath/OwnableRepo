f40087cdf49760d6b439acad4768dcdf7f4fb26c
dkozhedub <dkozhedub84@gmail.com>
2018-09-28 16:38:01 +0300

    defined UI contlrols

--- OwnableCI/Pages/SignInPage.cs
+++ OwnableCI/Pages/SignInPage.cs
@@ -11,6 +11,45 @@ namespace OwnableCI.Pages
 {
     class SignInPage : BasePage
     {
+        #region IWebelements
+
+        [FindsBy(How = How.XPath, Using = "//span[@class='auth0-lock-close-button']")]
+        public IWebElement btnClose;
+
+
+        [FindsBy(How = How.XPath, Using = "//a[text()='Log In']")]
+        public IWebElement tabLogIn;
+
+
+        [FindsBy(How = How.XPath, Using = "//a[text()='Sign Up']")]
+        public IWebElement tabSignUp;
+
+
+        [FindsBy(How = How.XPath, Using = "//div[text()='Log in with Facebook']")]
+        public IWebElement btnLogInWithFacebook;
+
+
+        [FindsBy(How = How.XPath, Using = "//div[text()='Log in with Google']")]
+        public IWebElement btnLogInWithGoogle;
+
+
+        [FindsBy(How = How.XPath, Using = "//input[@name='email']")]
+        public IWebElement inputEmail;
+
+
+        [FindsBy(How = How.XPath, Using = "//input[@name='password']")]
+        public IWebElement inputPassword;
+
+
+        [FindsBy(How = How.XPath, Using = "//a[@class='auth0-lock-alternative-link']")]
+        public IWebElement lnkDontRememberYourPassword;
+
+
+        [FindsBy(How = How.XPath, Using = "//button[@name='submit']/span[@class='auth0-label-submit']")]
+        public IWebElement btnLogIn;
+
+        #endregion
+
         private IWebDriver browser;
         public SignInPage(IWebDriver browser) : base(browser)
         {
