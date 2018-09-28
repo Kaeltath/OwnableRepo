d0e2af4fb8e6346a95afc80519228365d36b7f35
dkozhedub <dkozhedub84@gmail.com>
2018-09-28 12:45:17 +0300

    added SignIn page declaartion

---
+++ OwnableCI/Pages/SignInPage.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OwnableCI_TestLib.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnableCI.Pages
+{
+    class SignInPage : BasePage
+    {
+        private IWebDriver browser;
+        public SignInPage(IWebDriver browser) : base(browser)
+        {
+            this.browser = browser;
+            PageFactory.InitElements(driver, this);
+            NavigateToPage();
+        }
+
+        public override void NavigateToPage(string parameter = "")
+        {
+            new HomePage(browser).ClickbtnSignIn();
+            InitPage(this);
+        }
+    }
+}
