5b8eccca51a84b1973af37dddf8f02d303c43251
VadymPavliuk <elumielka@gmail.com>
2018-10-21 18:35:05 +0300

    Added Action method, to wrap test code for handling logs, and making screenshots on fails(screenshot code currently commented)

--- OwnableCI/Tests/BaseTest.cs
+++ OwnableCI/Tests/BaseTest.cs
@@ -1,19 +1,10 @@
 ﻿using OpenQA.Selenium;
-using OwnableCI_TestLib.Pages;
-using OwnableCI_TestLib.Constants;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.IE;
 using NUnit.Framework;
-using System.Threading;
-using OwnableCI.XMLParsers;
-using OwnableCI.TestDataObjs;
-using OwnableCI.Pages;
+using System.Diagnostics;
 
 namespace OwnableCI_TestLib.Tests
 {
@@ -24,10 +15,30 @@ namespace OwnableCI_TestLib.Tests
         internal log4net.ILog log = log4net.LogManager.GetLogger(typeof(BaseTest));
         internal IWebDriver driverForRun;
 
+        protected void TestAction(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception ex)
+            {
+                //StackTrace trace = new StackTrace();
+                //var screenshot = ((ITakesScreenshot)driverForRun).GetScreenshot();
+                //var filePath = String.Format(@"C:\Screenshots\{0}",trace.GetFrame(1).GetMethod().Name);
+
+                //screenshot.SaveAsFile(filePath, ScreenshotImageFormat.Png);
+
+                log.ErrorFormat("Exception occured in Test: {0}", ex.Message);
+
+                throw;
+            }
+        }
+
         [OneTimeSetUp]
         public void CreateBrowser()
         {
-            driverForRun = new ChromeDriver("D:\\SelTestLib\\OwnableCI_TestLib\\OwnableCI_TestLib\\Drivers");
+            driverForRun = new ChromeDriver("D:\\Downloads\\chromedriver_win32");
         }
 
         [OneTimeTearDown]
---
+++ OwnableCI/Tests/LoginsTest.cs
@@ -0,0 +1,138 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OwnableCI.Pages;
+using OwnableCI.TestDataObjs;
+using OwnableCI_TestLib.Constants;
+using OwnableCI_TestLib.Pages;
+using OwnableCI_TestLib.Tests;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OwnableCI.Tests
+{
+    [TestFixtureSource(typeof(TestProperties), "users")]
+    [TestFixture]
+    public class EmailValidationTest : BaseTest
+    {
+        private TestUser user;
+        private IWebElement confirmElement;
+        private bool isfirtLogin = true;
+
+        public EmailValidationTest(TestUser User)
+        {
+            user = User;
+        }
+
+
+        [Test]
+        [Order(1)]
+        public void FirstLogin()
+        {
+            TestAction(() =>
+            {
+                string currentTestName = "User Creation";
+                log.Debug("Starting " + currentTestName + " Test;");
+                log.Debug("For user " + user.FirstName + user.LastName + ";");
+                SignInPage page = new SignInPage(driverForRun);
+                page.Login(user);
+                Thread.Sleep(5000);
+                var emailValidationPanel = driverForRun.FindElements(By.XPath("//div[@class='email-validation-holder']"));
+                var emailValidationSkipButton = driverForRun.FindElements(By.XPath("//div[@class='email-validation-holder']//button[@text='Skip']"));
+                isfirtLogin = (emailValidationPanel.Count > 0 && emailValidationSkipButton.Count < 1);
+                Assert.That(isfirtLogin);
+            });
+        }
+
+        [Test]
+        [Order(2)]
+        public void EMailValidation()
+        {
+            TestAction(() =>
+            {
+                string currentTestName = "User Creation";
+                log.Debug("Starting " + currentTestName + " Test;");
+                log.Debug("For user " + user.FirstName + user.LastName + ";");
+                Assume.That(isfirtLogin, "Not first Login, not running the test");
+                Assert.That(ValidateEmail());
+            });
+        }
+
+        [Test]
+        [Order(3)]
+        public void UserLogin()
+        {
+            TestAction(() =>
+            {
+                string currentTestName = "User Creation";
+                log.Debug("Starting " + currentTestName + " Test;");
+                log.Debug("For user " + user.FirstName + user.LastName + ";");
+                SignInPage page = new SignInPage(driverForRun);
+                page.Login(user);
+                Thread.Sleep(5000);
+                confirmElement = driverForRun.FindElement(By.XPath("//div[@class='modal-body']"));
+                Assume.That(confirmElement != null, "Not a user, but member");
+            });           
+        }
+
+        [Test]
+        [Order(4)]
+        public void MemberLogin()
+        {
+            TestAction(() =>
+            {
+                string currentTestName = "User Creation";
+                log.Debug("Starting " + currentTestName + " Test;");
+                log.Debug("For user " + user.FirstName + user.LastName + ";");
+                SignInPage page = new SignInPage(driverForRun);
+                page.Login(user);
+                Thread.Sleep(5000);
+                confirmElement = driverForRun.FindElement(By.XPath("//a[@id='navbarDropdownMenuLink']"));
+
+                Assert.That(confirmElement.Text == String.Format("Hello, " + user.FirstName));
+            });
+        }
+
+        public bool ValidateEmail()
+        {
+            try
+            {
+                SignInPage page = new SignInPage(driverForRun);
+                driverForRun.Navigate().GoToUrl("http://gmail.com");
+                driverForRun.FindElement(By.Id("identifierId")).SendKeys(user.Email);
+                driverForRun.FindElement(By.Id("identifierNext")).Click();
+                Thread.Sleep(2000);
+                driverForRun.FindElement(By.XPath("//input[@name='password']")).SendKeys(user.Password);
+                driverForRun.FindElement(By.Id("passwordNext")).Click();
+                Thread.Sleep(3000);
+                driverForRun.FindElement(By.XPath("//div[@class='UI']//table/tbody/tr/td//div[@role='link']")).Click();
+                Thread.Sleep(1000);
+                driverForRun.FindElement(By.XPath("//a[contains(text(),'ownable.auth0.com')]")).Click();
+                Thread.Sleep(4000);
+                ReadOnlyCollection<string> windowHandles = driverForRun.WindowHandles;
+                driverForRun.SwitchTo().Window(windowHandles[1]);
+                driverForRun.Close();
+                driverForRun.SwitchTo().Window(windowHandles[0]);
+                page.NavigateToPage();
+                Thread.Sleep(4000);
+                page.Login(user);
+                Thread.Sleep(4000);
+                var element = driverForRun.FindElements(By.XPath("//div[@class='email-validation-holder']//button[@text='Skip']"));
+                if (element.Count > 0)
+                { element[0].Click(); }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+
+}
