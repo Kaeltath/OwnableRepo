5083a2d6338a82a6ca3a15e8c3c1545aca433a9f
VadymPavliuk <elumielka@gmail.com>
2018-11-20 02:50:45 +0200

    + TestHelper class that will contain general utility methods. + Updating project infra

---
+++ OwnableCI/SeviceClasses/TestHelper.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OwnableCI.ServiceClasses
+{
+    public static class TestHelper
+    {
+        public static void JSexecutorClick(IWebElement element, IWebDriver driver)
+        {
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("arguments[0].click()", element);
+        }
+
+        //public static void SmallSleep()
+        //{
+        //    Thread.Sleep(2000);
+        //}
+
+        //public static void MidSleep()
+        //{
+        //    Thread.Sleep(3500);
+        //}
+
+        //public static void BigSleep()
+        //{
+        //    Thread.Sleep(5000);
+        //}
+    }
+}
