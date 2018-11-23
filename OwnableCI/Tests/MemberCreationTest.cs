bc1d85b370489c44262616ef06f2808321d45af3
dkozhedub <dkozhedub84@gmail.com>
2018-11-23 13:36:06 +0200

    TC MemberCreationSignUp - code adjustment

--- OwnableCI/Tests/MemberCreationTest.cs
+++ OwnableCI/Tests/MemberCreationTest.cs
@@ -27,72 +27,53 @@ namespace OwnableCI.Tests
         [Order(1)]
         public void MemberCreationSignUp()
         {
-            TestAction(() =>
+            if (user.ExpResult == "Accept")
             {
-                string currentTestName = "Member Creation from SignUp";
-                log.Debug("Starting " + currentTestName + " Test;");
-                log.Debug("For user " + user.FirstName + user.LastName + ";");
-                SignUpPage pageSignUp = new SignUpPage(driverForRun);
-                MidSleep();
-                pageSignUp.inputEmail.SendKeys(user.Email);
-                pageSignUp.inputPassword.SendKeys(user.Password);
-                //pageSignUp.chkIAgreeToTheTerms.Click(); //removed from current version
-                pageSignUp.btnLogIn.Click();
-                log.Debug("Start get your rental cap");
-                MidSleep();
-                driverForRun.FindElement(By.XPath("//button/div[text()=' GET YOUR RENTAL CAP ']")).Click();
+                TestAction(() =>
+                {
+                    string currentTestName = "Member Creation from SignUp";
+                    log.Debug("Starting " + currentTestName + " Test;");
+                    log.Debug("For user " + user.FirstName + user.LastName + ";");
+                    SignUpPage pageSignUp = new SignUpPage(driverForRun);
+                    MidSleep();
+                    pageSignUp.UserSignUp(user);
+                    log.Debug("Start get your rental cap");
+                    BigSleep();
+                    BigSleep();
+                    driverForRun.FindElement(By.XPath("//button/div[text()=' GET YOUR RENTAL CAP ']")).Click();
 
-                //TODO: move set "Personal Info" to separate method
-                MidSleep();
-                MemberCreationFirstPage pagePersonalInfo = new MemberCreationFirstPage(driverForRun);
-                pagePersonalInfo.inputFirstName.SendKeys(user.FirstName);
-                pagePersonalInfo.inputLastName.SendKeys(user.LastName);
-                pagePersonalInfo.inputHomeAddress.SendKeys(user.Adress);
-                pagePersonalInfo.inputCity.SendKeys(user.City);
-                pagePersonalInfo.lstState.Click();
-                driverForRun.FindElement(By.XPath("//span[text()='Texas']")).Click();
-                pagePersonalInfo.inputZipCode.SendKeys(user.ZipCode);
-                pagePersonalInfo.inputMobile.SendKeys(user.Mobile);
-                pagePersonalInfo.inputBirthdate.SendKeys(user.BirthDate);
-                pagePersonalInfo.chkAgreement.Click();
-                pagePersonalInfo.btnNext.Click();
+                    MidSleep();
+                    MemberCreationFirstPage pagePersonalInfo = new MemberCreationFirstPage(driverForRun);
+                    pagePersonalInfo.lstState.Click();
+                    driverForRun.FindElement(By.XPath("//span[text()='Texas']")).Click(); //QQ: need to implement selection state inside SetPersonalInfo() method
+                    pagePersonalInfo.SetPersonalInfo(user);
 
-                //TODO: move set "Income Info" to separate method
-                MidSleep();
-                MemberCreationSecondPage pageIncomeInfo = new MemberCreationSecondPage(driverForRun);
-                pageIncomeInfo.inputMonthlyIncome.SendKeys(user.MonthlyIncome);
-                pageIncomeInfo.inputCompany.SendKeys(user.Company);
-                pageIncomeInfo.inputYearsEmployed.SendKeys(user.YearsEmployed);
-                pageIncomeInfo.inputSSN.SendKeys("12345"+user.LastDigitsOFSocial);
-                pageIncomeInfo.btnBecomeMember.Click();
+                    MidSleep();
+                    MemberCreationSecondPage pageIncomeInfo = new MemberCreationSecondPage(driverForRun);
+                    pageIncomeInfo.SetIncomeInfo(user);
 
-                //MEMBERSHIP AGREEMENT
-                MidSleep();
-                MemberCreationThirdPage pageMembershipAgreement = new MemberCreationThirdPage(driverForRun);
-                pageMembershipAgreement.btnAgree.Click(); //do this for going to txtMemberSignature field
-                SmallSleep();
-                pageMembershipAgreement.txtMemberSignature.Click(); //set digital signature
-                SmallSleep();
-                pageMembershipAgreement.btnAgree.Click();
+                    MidSleep();
+                    MemberCreationThirdPage pageMembershipAgreement = new MemberCreationThirdPage(driverForRun);
+                    pageMembershipAgreement.SetMembershipAgreement(user);
 
-                //APPLICATION DISCLOSURE
-                MidSleep();
-                MemberCreationFourthPage pageApplicationDisclosure = new MemberCreationFourthPage(driverForRun);
-                pageApplicationDisclosure.btnAgree.Click();
+                    MidSleep();
+                    MemberCreationFourthPage pageApplicationDisclosure = new MemberCreationFourthPage(driverForRun);
+                    pageApplicationDisclosure.btnAgree.Click();
 
-                //CONGRATULATIONS!
-                MidSleep();
-                MemberCreationFifthPage pageCongratulations = new MemberCreationFifthPage(driverForRun);
-                string sRentValue = pageCongratulations.txtRentalCapValue.Text;
-                Assert.AreEqual("$500.00", sRentValue);
-                pageCongratulations.btnStartShopping.Click();
+                    MidSleep();
+                    MemberCreationFifthPage pageCongratulations = new MemberCreationFifthPage(driverForRun);
+                    string sRentActualValue = pageCongratulations.txtRentalCapValue.Text;
+                    string sRentExpectedValue = RentalCapExpected(user);
+                    Assert.AreEqual(sRentExpectedValue, sRentActualValue); //QQ: how to make non-stop on this verification?
+                    pageCongratulations.btnStartShopping.Click();
 
-                //Home Page
-                MidSleep();
-                HomePage pageHome = new HomePage(driverForRun);
-                ValidateMember(user);
-                Assert.AreEqual("$500.00",pageHome.GetRentalCap());
-            });
+                    //Home Page
+                    MidSleep();
+                    //HomePage pageHome = new HomePage(driverForRun); //QQ: how to skip validation declared controls here
+                    ValidateMember(user);
+                    //Assert.AreEqual(sRentExpectedValue,pageHome.GetRentalCap());
+                });
+            }
         }
 
         [Test]
